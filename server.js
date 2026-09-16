33622789439d2df1804b8df53c88ced3dd7920e9
LuisMi04-30 <mendozayunesl@gmail.com>
2026-09-16 14:42:50 -0400

    Actualizar server.js

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require("express");
 
 const app = express();
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("🐾 Collar Inteligente - Servidor funcionando");
+});
+
+// Recibir ubicación del collar
+app.post("/ubicacion", (req, res) => {
+  const { latitud, longitud } = req.body;
+
+  console.log("📍 Ubicación recibida:");
+  console.log("Latitud:", latitud);
+  console.log("Longitud:", longitud);
+
+  const mapa = `https://www.google.com/maps?q=${latitud},${longitud}`;
+
+  res.json({
+    estado: "ok",
+    mensaje: "Ubicación recibida correctamente",
+    latitud: latitud,
+    longitud: longitud,
+    mapa: mapa
+  });
 });
 
 const PORT = process.env.PORT || 10000;
